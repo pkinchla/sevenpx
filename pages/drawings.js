1a4c0b71ae00ec2e44c98be8a9d978d6a3cca319
Paul Kinchla <pkinchla@gmail.com>
2022-06-14 23:16:07 -0400

    resolve trailing slashes and addition of drawings view

---
+++ pages/drawings.js
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <Head>
+      <title>Seven Pixels | Drawings</title>
+      <meta
+        name="description"
+        content="A list of vector drawings that can edited in the browser. as each drawing is updated the backround collage will update"
+      />
+    </Head>
+  );
+}
