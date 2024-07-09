c754362278557ddcc0ab5a5b8196fc87f0487275
Paul Kinchla <pkinchla@gmail.com>
2024-07-09 13:55:44 -0400

    some cleanup

--- islands/VectorCollage.tsx
+++ islands/VectorCollage.tsx
@@ -29,6 +29,7 @@ const Vector = () => {
   return (
     <div className="background">
       <svg
+        className="background-animation"
         aria-hidden="true"
         viewBox="0 0 1000 1000"
         xmlns="http://www.w3.org/2000/svg"
