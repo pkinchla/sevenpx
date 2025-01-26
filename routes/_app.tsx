36317be4fc634f69cb75f39f178a970f2bbed2c6
Paul Kinchla <pkinchla@gmail.com>
2025-01-26 11:08:22 -0500

    dont index for now

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -7,6 +7,7 @@ export default function App({ Component }: PageProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>sevenpx</title>
         <link rel="stylesheet" href="/styles.css" />
+        <meta name="robots" content="noindex"></meta>
       </head>
       <body>
         <Component />
