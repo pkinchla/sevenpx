1f040bd8d8da8ca18e2dc3d1620ecf542276bc08
Paul Kinchla <pkinchla@gmail.com>
2025-02-16 18:17:23 -0500

    added lang and removed typekit

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -12,7 +12,7 @@ export const handler: Handlers = {
 };
 export default function App({ Component, route }: PageProps) {
   return (
-    <html>
+    <html lang="en-US">
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
